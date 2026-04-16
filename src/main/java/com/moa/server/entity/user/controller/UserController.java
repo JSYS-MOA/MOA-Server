2118491379916a18940293c5b63ba61da7f8e542
박준 <157086374+mutajune@users.noreply.github.com>
2026-04-16 16:48:48 +0900

    Update UserController.java

--- src/main/java/com/moa/server/entity/user/controller/UserController.java
+++ src/main/java/com/moa/server/entity/user/controller/UserController.java
@@ -7,11 +7,10 @@ import com.moa.server.entity.user.service.UserService;
 import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
 
 
 @RestController
