e2950e5a12abfa1a4328d930b5f95e77ee7381d7
박준 <157086374+mutajune@users.noreply.github.com>
2026-04-23 09:44:15 +0900

    Update AllowanceRepository.java

--- src/main/java/com/moa/server/entity/salary/AllowanceRepository.java
+++ src/main/java/com/moa/server/entity/salary/AllowanceRepository.java
@@ -1,10 +1,8 @@
 package com.moa.server.entity.salary;
 
-import com.moa.server.entity.user.AdminRoleEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.lang.ScopedValue;
 import java.util.Optional;
 
 @Repository
