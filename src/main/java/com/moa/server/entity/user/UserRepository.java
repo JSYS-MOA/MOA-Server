d0ffb3207cd2945429058e556ce0908ef708e249
박준 <157086374+mutajune@users.noreply.github.com>
2026-04-23 11:25:35 +0900

    .

--- src/main/java/com/moa/server/entity/user/UserRepository.java
+++ src/main/java/com/moa/server/entity/user/UserRepository.java
@@ -1,7 +1,6 @@
 package com.moa.server.entity.user;
 
 import com.moa.server.entity.inventory.InventoryEntity;
-import com.moa.server.entity.user.dto.AdminUserDTO;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.EntityGraph;
