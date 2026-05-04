27e61939100af1a8d41220b2d79bf143c4791b95
박준 <157086374+mutajune@users.noreply.github.com>
2026-05-04 17:00:16 +0900

    maping edit

--- src/main/java/com/moa/server/entity/layout/dto/LayoutDTO.java
+++ src/main/java/com/moa/server/entity/layout/dto/LayoutDTO.java
@@ -4,6 +4,7 @@ import com.moa.server.entity.menu.MenuEntity;
 import lombok.*;
 
 import java.util.List;
+import java.util.Map;
 
 @Getter
 @Setter
@@ -17,6 +18,6 @@ public class LayoutDTO {
     private String departmentName;
     private String gradeName;
 
-    private List<MenuEntity> menuList;
+    private List<Map<String, Object>> menuList;
 
 }
