584958eacabe6dbb507c2c39688e1e948b943439
박준 <157086374+mutajune@users.noreply.github.com>
2026-05-06 14:54:13 +0900

    Update SecurityConfig.java

--- src/main/java/com/moa/server/config/SecurityConfig.java
+++ src/main/java/com/moa/server/config/SecurityConfig.java
@@ -60,7 +60,7 @@ public class SecurityConfig {
                 "https://*.vercel.app"
         ));
 
-        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
 
